66450b60212c60830aa52b138470bb8f1093010c
hiro21 <hiswing8@gmail.com>
2021-09-27 21:11:48 +0900

    add boolean

---
+++ chapter-3/src/sampleBoolean.ts
@@ -0,0 +1,13 @@
+// 1.値がbooleanであることを推論させる使い方
+let booleanTestLet = true
+var booleanTestVar = false
+
+// 2.値が特定のbooleanであることを推論させる使い方
+const booleanTestConst = true
+
+// 3.値がbooleanであることを明示的にする使い方
+let booleanTestExpressly: Boolean = true
+
+// 4.値が特定のbooleanであることを明示的にする使い方
+let booleanCompareResult: true = true
+// let booleanCompareCompileError: true = false
