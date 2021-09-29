e68491218b2acb044eca8470802efa33d287371b
hiro21 <hiswing8@gmail.com>
2021-09-29 19:04:47 +0900

    add String

---
+++ chapter-3/src/sampleString.ts
@@ -0,0 +1,7 @@
+let testString1 = 'hello'
+var testString2 = 'world'
+const testString3 = '!'
+let addString = a + ' ' + b + c
+let testString4: 'john' = 'john'
+// 以下はエラーになる
+// let testString5: 'john' = 'zoe'
